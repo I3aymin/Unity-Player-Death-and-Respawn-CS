ea4e4ba9a0be09489e35e8aaf4b262420d9dc23e
Everett Ramos <82851336+I3aymin@users.noreply.github.com>
2021-07-13 18:49:13 -0500

    Add files via upload

---
+++ PlayerDeath.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDeath : MonoBehaviour
+{
+    public float jumpTimer = 3.5f;
+    public float autoJump = 3.5f;
+    public Vector3 startPosition; 
+
+    private void Awake()
+    {
+        startPosition = transform.position;
+    }
+
+    //Checks if the player character has made contact with a spike and sends the information for respawning
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Spike"))
+        {
+            transform.position = startPosition;
+            //LevelManager.instance.Respawn();
+        }
+    }
+}
